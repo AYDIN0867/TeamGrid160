b5b17c24e73b75d0111ecee8164d02f6061a9d26
ramazan <coolstoves@gmail.com>
2025-08-14 16:59:17 +0300

    140825659

---
+++ src/test/java/stepDefinitions/Grid_01.java
@@ -0,0 +1,35 @@
+package stepDefinitions;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+
+public class Grid_01 {
+
+    //testi koşabilmek için driver ı tanımlarım
+    //static yaptım çünkü diğer methodlarda da kullanmak isityorum
+
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+
+       driver=new RemoteWebDriver(new URL(""),new)
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+}
