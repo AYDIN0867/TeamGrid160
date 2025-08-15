953a0557378a8b747f891e2ffc0fe301c207a360
ramazan <coolstoves@gmail.com>
2025-08-15 21:03:41 +0300

    1508252103

---
+++ src/test/java/stepDefinitions/GoogleStepDefinitions.java
@@ -0,0 +1,32 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class GoogleStepDefinitions {
+
+
+    @Given("Kullanici Url'e gider.")
+    public void kullanici_url_e_gider() {
+
+
+    }
+
+    @Given("Kullanici Url'i dogrular.")
+    public void kullanici_url_i_dogrular() {
+
+
+    }
+
+    @Given("Kullanici sayfayi kapatir.")
+    public void kullanici_sayfayi_kapatir(){
+
+
+
+
+
+
+    }
+
+
+
+}
---
+++ src/test/resources/google.feature
@@ -0,0 +1,9 @@
+Feature: Google title dogrulama testi.
+
+  Scenario: Title dogrulama.
+
+    * Kullanici Url'e gider.
+    * Kullanici Url'i dogrular.
+    * Kullanici sayfayi kapatir.
+
+    #genellikle dogrulama adımları then de yazılır...
